72a1dda2593dcb0188629ea8334b9bbf27bdd5a6
arthur109 <arthurfaj@gmail.com>
2019-04-12 18:06:50 -0700

    turd stuff

--- sketch.js
+++ sketch.js
@@ -2,7 +2,8 @@
 function setup() {
   createCanvas(500,500);
   fill(255,0,0);
-  ellipse(250,250,100,100)
+  ellipse(250,250,100,100);
+  text("I am Turd Man, and I like to fart...A lot.",100,10);
 }
 
 function draw() {
