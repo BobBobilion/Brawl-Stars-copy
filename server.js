ee137b72dbd2a696cabac1b7a23340f7c80de88a
arthur109 <arthurfaj@gmail.com>
2019-04-13 18:59:35 -0700

    Simple drawing, fully operational
    need to npm install socket.io

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 var express = require('express');
+
+var socket = require('socket.io');
+
 var app = express();
 var server = app.listen(3000);
-
 app.use(express.static('public'));
-
 console.log("hello");
+
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+
+function newConnection(socket){
+    console.log("new connection: " + socket.id);
+    socket.on('mouse',onMouseMoved);
+    function onMouseMoved(data){
+        console.log(data);
+        socket.broadcast.emit('newPoint',data);
+    }
+}
+
